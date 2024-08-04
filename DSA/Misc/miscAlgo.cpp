f08a43b3102e7e1ca79f22db05f8bea46523aa5c
Prashant Kashyap <prashant.kashyap2002@gmail.com>
2024-08-04 14:12:03 +0000

    fibonacci series implementation

--- DSA/Misc/miscAlgo.cpp
+++ DSA/Misc/miscAlgo.cpp
@@ -2,9 +2,12 @@
 
 #if 1
 int main () {
-  int num = 5;
+  int num = 7;
   std::cout << "recursive-factorial of num:" << num << " is " << factorialRecursion(num) << std::endl;
-  std::cout << "itertative-factorial of num:" << num << " is " << factorialIterative(num);
+  std::cout << "itertative-factorial of num:" << num << " is " << factorialIterative(num) << std::endl;
+
+  std::cout << "Rec-fib of num:" << num << " is "<< fibRecursion(num) << std::endl;
+  std::cout << "Iter-fib of num:" << num << " is "<< fibIterative(num) << std::endl;
     
 }
 
--- DSA/Misc/miscAlgo.h
+++ DSA/Misc/miscAlgo.h
@@ -1,20 +1,47 @@
 #include <iostream>
+#include <vector>
 
-int factorialRecursion (int num) {
-  if (num <=0) return 0;
-  
+int factorialRecursion(int num) {
+  if (num <= 0)
+    return 0;
+
   if (num == 1)
     return num;
 
-  return (num * factorialRecursion(num-1));
+  return (num * factorialRecursion(num - 1));
 }
 
-int factorialIterative (int num) {
-  if (num <=0) return 0;
-  
+int factorialIterative(int num) {
+  if (num <= 0)
+    return 0;
+
   int result = 1;
   for (int i = 1; i <= num; i++) {
     result *= i;
   }
   return result;
+}
+
+int fibRecursion(int num) {
+  if (num <= 1) {
+    return 0;
+  }
+  if (num == 2) {
+    return 1;
+  }
+  return (fibRecursion(num - 1) + fibRecursion(num - 2));
+}
+
+int fibIterative (int num) {
+  if (num <= 1) {
+    return 0;
+  }
+  if (num == 2) {
+    return 1;
+  }
+  std::vector<int> arr{0,1};
+  for (int i = 2; i <num; i++) {
+    arr.push_back(arr[i-1] + arr[i-2]);
+  }
+  return arr[num-1];
 }
